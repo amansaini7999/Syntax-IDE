e163a46b6d3600aa0f617d2a8b7170c1a8ef7b1d
Aman Saini <amnsaini@amazon.com>
2025-05-10 19:06:16 +0530

    Initial Swift project setup

---
+++ Syntax-IDE/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Syntax-IDE
+//
+//  Created by Saini, Aman on 10/05/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
